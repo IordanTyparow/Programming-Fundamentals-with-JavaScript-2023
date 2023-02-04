734497584e18d43f6c11691477175da4381e70b6
IordanTyparow <120342477+IordanTyparow@users.noreply.github.com>
2023-02-04 16:54:34 +0200

    exam

--- fundamentals/tasks/48.LadyBugs.js
+++ fundamentals/tasks/48.LadyBugs.js
@@ -1,32 +1,62 @@
 function ladyBugs(input) {
 
-    let sizeOfField = input.shift();
-    let startedIndexes = input.shift().split(' ').map(Number);
+    let field = [];
+    let size = Number(input[0]);
 
-    for (let i = 0; i < input.length; i++) {
+    for (let i = 0; i < size; i++) {
+        field[i] = 0;
+    }
+
+    let indexOfLadyBugs = input[1].split(' ');
+
+    for (let i = 0; i < indexOfLadyBugs.length; i++) {
+        let index = indexOfLadyBugs[i];
+        if (field[index] !== undefined) {
+            field[index] = 1;
+        }
+    }
+    
+    for (let i = 2; i < input.length; i++) {
         let commands = input[i].split(' ');
-
-        let ladyBugIndex = Number(commands[0]);
-        let currentMove = commands[1];
+        let indexOfLadyBug = Number(commands[0]);
+        let direction = commands[1];
         let flyLength = Number(commands[2]);
 
-        if (currentMove === 'right') {
-            if (flyLength < sizeOfField) {
-                if (ladyBugIndex >= 0 && flyLength >= 0) {
-                    if (startedIndexes[ladyBugIndex] !== 1) {
-                        startedIndexes[flyLength + 1] = 1;
-                    } else {
-                        startedIndexes[ladyBugIndex] = 0;
-                    }
+        if (indexOfLadyBug >= field.length || indexOfLadyBug < 0 || field[indexOfLadyBug] === 0)  {
+            continue;
+        }
+        
+        field[indexOfLadyBug] = 0;
+        let isFly = true;
+
+        if (direction === 'right') {
+            while(isFly) {
+                if (field[indexOfLadyBug + flyLength] === 0) {
+                    field[indexOfLadyBug + flyLength] = 1;
+                    isFly = false;
+                } else if (field[indexOfLadyBug + flyLength] === 1) {
+                    flyLength += flyLength;
+                } else {
+                    isFly = false;
+                }
+            }
+        } else {
+            while(isFly) {
+                if (field[indexOfLadyBug - flyLength] === 0) {
+                    field[indexOfLadyBug - flyLength] = 1;
+                    isFly = false;
+                } else if (field[flyLength] === 1) {
+                    flyLength += flyLength;
+                } else {
+                    isFly = false;
                 }
             }
         }
     }
-    console.log(startedIndexes.join(' '));
+    console.log(field.join(' '));
 }
 
-ladyBugs([3, '0 1 2',
-    '0 right 1',
-    '1 right 1',
-    '2 right 1']
+ladyBugs([ 5, '3',
+'3 left 2',
+'1 left -2']
 );
